2bbbf238093c055d2be8c4005a7389f259435642
seda <seda.diriker@gmail.com>
2024-07-22 10:55:04 +0300

    user ve meeting modelleri oluşturuldu

--- obj/Debug/net8.0/Meet-Manager.AssemblyInfo.cs
+++ obj/Debug/net8.0/Meet-Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meet-Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ccb69e55bb607531e66ea107d94037de69290e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd1be596edcf5baa1370c976ae18583aadb84cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meet-Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meet-Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Models/User.cs
@@ -0,0 +1,13 @@
+namespace Meet_Manager.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ProfilePicture { get; set; }
+    }
+}
